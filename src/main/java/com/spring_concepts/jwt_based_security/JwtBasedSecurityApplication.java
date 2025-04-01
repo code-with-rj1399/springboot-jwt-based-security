1695feb03841a3bdfb6dd0361d003dc0dd28f1a7
Rajesh <rajesh4.ramesh@gmail.com>
2025-04-01 11:54:35 +0530

    project changes

---
+++ src/main/java/com/spring_concepts/jwt_based_security/JwtBasedSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.spring_concepts.jwt_based_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtBasedSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtBasedSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_concepts/jwt_based_security/JwtBasedSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_concepts.jwt_based_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtBasedSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
